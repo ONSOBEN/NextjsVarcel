5c0b1440327ccc71df0db59a0ad07944fadc8f8b
ben <sobenkh35@gmail.com>
2024-03-31 10:03:49 +0700

    build page

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-5 shadow-lg rounded-lg bg-white">
+        <h1 className="text-2xl font-bold mb-5 text-center">Welcome to the Home Page</h1>
+        <p className="text-center mb-5">This is a simple Home page built with Next.js and styled with Tailwind CSS.</p>
+      </div>
+    </div>
       <button onClick={() => router.push("/about")}>about</button>
     </main>
   );
